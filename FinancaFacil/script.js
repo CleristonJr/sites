8142ea39f406eb44f1390e5a5b3a92a0d36551ae
CleristonJr <79161552+CleristonJr@users.noreply.github.com>
2023-09-19 15:43:09 -0300

    Adicionando gráfico

--- FinancaFacil/script.js
+++ FinancaFacil/script.js
@@ -1,3 +1,53 @@
+let stockData = {
+    labels: [],
+    datasets: [{
+        label: 'Estoque',
+        data: [],
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1
+    },
+    {
+        label: 'Saídas',
+        data: [],
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1
+    }]
+};
+
+let stockChart;
+
+function initializeChart() {
+    const ctx = document.getElementById('stockChart').getContext('2d');
+    stockChart = new Chart(ctx, {
+        type: 'line',
+        data: stockData,
+        options: {
+            scales: {
+                x: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Data'
+                    }
+                }],
+                y: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Quantidade'
+                    }
+                }]
+            }
+        }
+    });
+}
+
+function updateChart() {
+    stockChart.update();
+}
+
 function calculateTotal() {
     const itemQuantity = parseFloat(document.getElementById('itemQuantity').value);
     const itemPrice = parseFloat(document.getElementById('itemPrice').value);
@@ -15,6 +65,16 @@ function calculateSale() {
         return;
     }
 
+    const currentDate = new Date().toLocaleDateString();
+    stockData.labels.push(currentDate);
+    stockData.datasets[0].data.push(itemQuantity);
+    stockData.datasets[1].data.push(quantityToSell);
+
     const newTotalValue = (itemQuantity - quantityToSell) * parseFloat(document.getElementById('itemPrice').value);
     document.getElementById('newTotal').innerText = `Novo Total em Estoque: R$ ${newTotalValue.toFixed(2)}`;
+
+    updateChart();
 }
+
+// Chamada para inicializar o gráfico ao carregar a página
+initializeChart();
